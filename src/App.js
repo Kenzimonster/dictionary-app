57fb2f118572750c12b56750262f3b88326a69c6
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-11-14 08:02:13 +1100

    made resposity and added image to ensure everything is working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import BattleSisterClear from "./BattleSisterClear.png";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container" >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row">
+        <div className="col-3">
+        <img src={BattleSisterClear} className="App-logo img-fluid" alt="Sister of Battle reading a book" />
+        </div>
+        <div className="col-9"><h1>Inquisition Approved Dictionary</h1></div>
+        </div>
       </header>
     </div>
   );
