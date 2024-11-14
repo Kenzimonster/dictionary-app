2763468c48ed34220e592a1400eeb64d1329a2f5
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-11-14 14:29:49 +1100

    added synonyms to meanings
    added synonyms component to meaning to render the synonyms for api call
    adjusted some style to make site easier to read

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Meanings.css";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props){
   return (
   <div className="meaning">
     <h3>{props.meaning.partOfSpeech}</h3>
-    <p>{props.meaning.definition}</p>
-    <p><em>"{props.meaning.example}"</em></p>
+    <p><strong>Definition: </strong>{props.meaning.definition}</p>
+    <p><strong>Example: </strong><em>"{props.meaning.example}"</em></p>
+    <p><strong>Synonyms: </strong><Synonyms synonyms={props.meaning.synonyms}/></p>
   </div>)
 }
--- src/Results.js
+++ src/Results.js
@@ -7,7 +7,7 @@ export default function Results(props){
     return (
       <div className="results">
         <h2 className="text-capitalize">{props.results.word}</h2>
-        <p>/ {props.results.phonetic} /</p>
+        <p className="phonetics">/ {props.results.phonetic} /</p>
         {props.results.meanings.map(function (meaning, index){
           return <div key={index}>
              <Meanings  meaning={meaning} />
