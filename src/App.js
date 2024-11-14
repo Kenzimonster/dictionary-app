5ed2b843f6681a336279e000e8025d8ab351a01a
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-11-14 11:29:11 +1100

    added search form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import BattleSisterClear from "./BattleSisterClear.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
-    <div className="App container" >
+    <div className="App" >
+      <div className ="container">
       <header className="App-header">
         <div className="row">
-        <div className="col-3">
-        <img src={BattleSisterClear} className="App-logo img-fluid" alt="Sister of Battle reading a book" />
-        </div>
-        <div className="col-9"><h1>Inquisition Approved Dictionary</h1></div>
+          <div className="col-3">
+            <img src={BattleSisterClear} className="App-logo img-fluid" alt="Sister of Battle reading a book" />
+          </div>
+          <div className="col-9">
+            <div>
+              <h1>Inquisition Approved Dictionary</h1>
+            </div>
+            <div>
+             <Dictionary />
+           </div>
+          </div>
         </div>
       </header>
+      <footer>
+        coded by Kenzi👺
+      </footer>
+    </div>
     </div>
   );
 }
-
-export default App;
