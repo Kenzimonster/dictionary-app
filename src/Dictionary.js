1a9da032e5dc9f9b22b9a9470cf96bcaa244378b
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-11-14 11:48:10 +1100

    added api call to project

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
   let [keyword, setKeyword] = useState(" ");
 
+  function handleResponse(response){
+    console.log(response.data)
+  }
   function search(event){
     event.preventDefault();
-    alert(`Searching for ${keyword}`)
+    let apiKey =`3at0foeb77eba84a5c21cf21f38b13e9`
+    let query = keyword;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${query}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event){
     setKeyword(event.target.value);
